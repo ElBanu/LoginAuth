d5d62d4dcf8057494b200890d3f3017c4daf2276
ElBanu <andreibanu97@yahoo.com>
2022-07-15 13:45:34 +0300

    env added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -6,7 +7,7 @@ const app = express();
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
-const uriAtlas = "mongodb+srv://admin:admin@cluster0.vbbdz.mongodb.net/?retryWrites=true&w=majority";					
+const uriAtlas = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@cluster0.vbbdz.mongodb.net/?retryWrites=true&w=majority";					
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -30,9 +31,7 @@ const userSchema = new mongoose.Schema ({
     }					
     });
 
-
-const secret = "This is a long unguessable string that is used as a secret encryption method.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema);					
 
